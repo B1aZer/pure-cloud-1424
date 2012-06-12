7212cad9b5ae3ba498d09c63d9724696588a4441
B1aZer <blaze.imba@gmail.com>
2012-06-12 14:19:32 +0400

    inital fix

--- settings.py
+++ settings.py
@@ -9,12 +9,12 @@
 
 # Controls the ordering and grouping of the admin menu.
 #
-# ADMIN_MENU_ORDER = (
-#     ("Content", ("pages.Page", "blog.BlogPost",
-#        "generic.ThreadedComment", ("Media Library", "fb_browse"),)),
-#     ("Site", ("sites.Site", "redirects.Redirect", "conf.Setting")),
-#     ("Users", ("auth.User", "auth.Group",)),
-# )
+ADMIN_MENU_ORDER = (
+     ("Content", ("pages.Page", "blog_fork.BlogPost",
+        "generic.ThreadedComment", ("Media Library", "fb_browse"),)),
+     ("Site", ("sites.Site", "redirects.Redirect", "conf.Setting")),
+     ("Users", ("auth.User", "auth.Group",)),
+ )
 
 # A three item sequence, each containing a sequence of template tags
 # used to render the admin dashboard.
@@ -249,7 +249,6 @@ INSTALLED_APPS = (
     "mezzanine.twitter",
     #"mezzanine.accounts",
     #"mezzanine.mobile",
-    'django_extensions',
 )
 
 # List of processors used by RequestContext to populate the context.
