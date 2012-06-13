ad4e0e7a02fbb40ba28b14cfbaa7ae5386a3e70d
B1aZer <blaze.imba@gmail.com>
2012-06-13 16:04:11 +0400

    tweets

--- settings.py
+++ settings.py
@@ -249,7 +249,7 @@ INSTALLED_APPS = (
     "mezzanine.forms",
     "mezzanine.pages",
     "mezzanine.galleries",
-    "mezzanine.twitter",
+    #"mezzanine.twitter",
     #"mezzanine.accounts",
     #"mezzanine.mobile",
 )
