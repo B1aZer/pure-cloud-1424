088dd6ce9df444c1827e056360e128b4e4aafb34
B1aZer <blaze.imba@gmail.com>
2012-06-30 17:29:30 +0400

    heroku

--- settings.py
+++ settings.py
@@ -80,7 +80,7 @@ BLOG_USE_FEATURED_IMAGE = True
 # If True, the south application will be automatically added to the
 # INSTALLED_APPS setting. This setting is not defined in
 # mezzanine.conf.defaults as is the case with the above settings.
-USE_SOUTH = False
+USE_SOUTH = True
 
 
 ########################
